6a31be7475971a30a88bfe234443e3b6c5eabb18
Christian <cteguimtu620@gmail.com>
2024-08-10 07:57:56 -0400

    Final Struct

--- structs.playground/Contents.swift
+++ structs.playground/Contents.swift
@@ -11,18 +11,18 @@ struct Album {
 }
 
 let red = Album(title: "Red", artist: "Swift", year: 2012)
-//print(red.artist)
+print(red)
 
 //red.printSummary()
 
 struct Employee {
-    let name: String
-    var vacationRemaining: Int
+    let name: String  // here we can specifiy a default value like "Anonymous" but since it a constant, it would not be mutable when calling the instance of employee
+    var vacationRemaining: Int = 14  //here since vacationRemaining is a variable, it can have a default value so it dosent have to be specified when calling the struct Employee
 
    mutating func takeVacation(days: Int) {
         if vacationRemaining > days {
             vacationRemaining -= days
-            // all the data inside of a struct is constant becuz structs are constants by defauult which mean we cannot modifiy name and vacationRemaining. IN order for a functiont modify a struct, we need to name the function " mutating Func"
+            // all the data inside of a struct is constant becuz structs are constants by defauult which mean we cannot modifiy name and vacationRemaining. IN order for a functiont modify data in a struct, we need to name the function " mutating Func".
             print("I'm going on vacation!")
             print("Days remaining: \(vacationRemaining)")
         } else {
@@ -31,6 +31,8 @@ struct Employee {
     }
 }
 
-var archer = Employee(name: "Chris Teg", vacationRemaining: 20)
+var chris = Employee.init(name: "Chris Teg", vacationRemaining: 20)
+var ange = Employee(name: "Ange Teg")
 
-archer.takeVacation(days: 18)
+ange.takeVacation(days: 15)
+chris.takeVacation(days: 18)
