2101f62b15608f4de5b5241acce0177edef0d728
Christian <cteguimtu620@gmail.com>
2024-08-10 17:35:02 -0400

    didSet and willSet function

--- propertyObserver.playground/Contents.swift
+++ propertyObserver.playground/Contents.swift
@@ -1,15 +1,46 @@
 import Cocoa
 
 struct Game {
-    var score = 0
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
 }
 
 var game = Game()
 game.score += 10
-print("Score is now \(game.score)")
+
 game.score -= 3
-print("Score is now \(game.score)")
+
 game.score += 1
 
 // here we didnt print the socre after the last change and that is a possible mistake that can lead to bugs
 //we canfix that with 
+
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        //willSet
+        ///Purpose: willSet is called just before the value of the property is changed.
+        ///Parameters: It has a default parameter newValue that contains the value that will be set.
+        
+        didSet {
+            print("There are now \(contacts.count) contacts")
+            print("Old value was: \(oldValue)")
+            //didSet
+            ///Purpose: didSet is called immediately after the value of the property has been changed.
+            ///Parameters: It has a default parameter oldValue that contains the previous value before the change.
+        
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Chris Teg")
+
