c77c88aeb985c8b0f98bf938063e93046683d178
Christian <cteguimtu620@gmail.com>
2024-08-10 09:16:44 -0400

    get and set

--- computeProtertiesDynamically.playground/Contents.swift
+++ computeProtertiesDynamically.playground/Contents.swift
@@ -8,9 +8,9 @@ struct Employee {
 var archer = Employee(name: "Sterling Archer", vacationRemaining: 14)
 
 archer.vacationRemaining -= 5
-print(archer.vacationRemaining)
+//print(archer.vacationRemaining)
 archer.vacationRemaining -= 3
-print(archer.vacationRemaining)
+//print(archer.vacationRemaining)
 
 //This code is correct but we losee the initial value of vacationRemaining
 
@@ -20,13 +20,23 @@ struct employee {
     var vacationTaken = 0
     
     var remaining: Int {
-        vacationAllocated - vacationTaken
+        get {
+            vacationAllocated - vacationTaken
+        }
+        set {
+            vacationAllocated = vacationTaken + newValue //newValue is the value assigned to remaining
+        }
     }
     //"remaining" is a computed property as it runs a a calculation automatically whenever its called
 }
 
 var chris = employee(person: "Chris", vacationAllocated: 20)
 chris.vacationTaken += 10
-print(chris.remaining)
+chris.remaining = 2 
 print(chris.vacationAllocated)
+
+// a getter(get) reads info and is used to define what happens when we get values
+// a setter is used to define what happens when you change (or "set") the value of the property. You can customize how the new value is handled or how it affects other properties
+//The logic now is that if we take 10 days of vacation and then we have four days remaining, vacationAllocated = 14
+
 
