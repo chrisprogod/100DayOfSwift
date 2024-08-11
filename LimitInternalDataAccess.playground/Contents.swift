1102fa4053a831fd055ba648ac2e44f875a58a20
Christian <cteguimtu620@gmail.com>
2024-08-10 21:46:29 -0400

    private keyword

--- LimitInternalDataAccess.playground/Contents.swift
+++ LimitInternalDataAccess.playground/Contents.swift
@@ -1,7 +1,7 @@
 import Cocoa
 
 struct BankAccount {
-    var funds = 0
+    private(set) var funds = 0
     
     
     mutating func deposit(amount: Int) {
@@ -30,6 +30,9 @@ if success == true {
     print("Failed to withdraw money.")
 }
 
+print(myAccount.funds)
+
 //this code is correct but nothing stops us from messing with the value of our funds
 // like this funds -= 1000, like withdrawing 1000 when we dont necessarly have it
 
+//for this reason, we have to make the the fund proprety only availble to read and write inside the struct. It will only be modifiable inside of the structure. for that we use the keyword private
