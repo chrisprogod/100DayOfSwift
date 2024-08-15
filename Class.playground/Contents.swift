374758d3dc045ff82941ded7f6aab76fec3e6386
Christian <cteguimtu620@gmail.com>
2024-08-15 18:12:08 -0400

    .

--- Class.playground/Contents.swift
+++ Class.playground/Contents.swift
@@ -1,3 +1,13 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+class Game {
+    var score = 0 {
+        didSet {
+     
+            print("Score is now \(score)")
+        }
+    }
+}
+//the only difference between this class and a struct is the word "class" because we could swtich it out to "struct" and it would still work.
+var newGame = Game()
+newGame.score += 10
