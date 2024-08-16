d766b4504c92363b10747dceb3285718d79c34a2
Christian <cteguimtu620@gmail.com>
2024-08-16 09:28:22 -0400

    init

--- ClassInheritance.playground/Contents.swift
+++ ClassInheritance.playground/Contents.swift
@@ -1,3 +1,43 @@
+
 import Cocoa
 
-var greeting = "Hello, playground"
+class Employee {
+     let hours: Int
+    
+    init(hours: Int) {
+        self.hours = hours
+    }
+    
+    func printSummary() {
+        print("I worked \(hours) hours today.")
+    }
+}
+
+//class dont automatically create memberwise initializer so we always need a custom one
+
+class Developer: Employee {
+    func work() {
+        print("I'm writing code for \(hours) hours.")
+    }
+    
+    override func printSummary() {   // we use override to change a method from a superclass(parentclass)
+                                     //if a parent class has a method that dosent take a parameter and dosent return anything and the parent class has child class with the same emthod that takes in a paramteter, the methods are considered different and wont require the override keyword.
+        print("As a Dev, I worked \(hours) hours today. ")
+    }
+}
+                                                            // Developer and Manager both inherit the Employee propertie (hours) and its initializer
+
+final class Manager: Employee {  //we can use final to identify a class that wont have any subclass
+    func work() {
+        print("I'm going to meetings for \(hours) hours.")
+    }
+}
+
+let robert = Developer(hours: 3)
+let marc = Manager(hours: 5)
+
+robert.work()
+marc.work()
+
+robert.printSummary() // we can call the function printSUmmary from the superclass Emloyee
+
--- ClassInit.playground/Contents.swift
+++ ClassInit.playground/Contents.swift
@@ -1,3 +1,25 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+class Vehicle {
+    let isElectric: Bool
+
+    init(isElectric: Bool) {
+        self.isElectric = isElectric
+    }
+}
+
+
+class Car: Vehicle {
+    var isConvertible: Bool = true
+
+      
+//    init(isConvertible: Bool, isElectric: Bool) {
+//        self.isConvertible = isConvertible
+//        super.init(isElectric: isElectric) // keyword super allows us to call from parent class
+                                            // if a child class has any custom initializers, it must always call the parent’s initializer after it has finished setting up its own properties, if it has any.
+        
+        //if a child class dosent have its own initiailyzer (had a default value for its property) it automatically inherit the initiailizer of its parent class
+}
+
+let teslaX = Car(isElectric: true)
+teslaX.isConvertible = false
