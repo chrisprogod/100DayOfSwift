78191080f3fb8a8054ab5f4b884eb57708210e2b
Christian Teguim <148562370+chrisprogod@users.noreply.github.com>
2024-09-11 09:34:48 -0400

    Car struct conforms to vehicule protocol

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -1,3 +1,24 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+protocol Vehicule {
+    func estimateTime(for distance: Int) -> Int
+    //no function body allowed
+    func travel(distance: Int)
+}
+
+struct Car: Vehicule {
+    func estimateTime(for distance: Int) -> Int {
+        distance/50
+    }
+    
+    func travel(distance: Int) {
+        print("I'm driving \(distance) km")
+    }
+    
+    func openSunroff() {
+        print("It's a nice day")
+    }
+}
+
+
+//because the struct Car conforms to the protocol Vehicule, it must have at least all of it methods. Car must contain both func estimateTime with the same parameter and return type and func travel(distance: Int)
